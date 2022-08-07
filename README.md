2e83d0e909f935ff5bd45a6ba101d7c98458df11
jmaczan <jedrzejpawel@maczan.pl>
2022-08-08 00:16:23 +0200

    ICMP ping work in progress

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # pff
 Examine Internet connection quality and status in terminal
+
+## Troubleshooting
+Issue:
+```
+thread 'main' panicked at 'called `Result::unwrap()` on an `Err` value: Os { code: 1, kind: PermissionDenied, message: "Operation not permitted" }', src/main.rs:12:25
+note: run with `RUST_BACKTRACE=1` environment variable to display a backtrace
+```
+
+Solution:
+`sudo setcap cap_net_raw+ep ./target/debug/pff`
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,59 @@
+use http::Request;
+use icmp;
+use reqwest::blocking;
+use std::collections::HashMap;
+use std::net::{IpAddr, Ipv4Addr};
+use std::net::{SocketAddr, ToSocketAddrs};
+use std::time::SystemTime;
+
 fn main() {
-    println!("Hello, world!");
+    // println!("Hello, world!");
+    // let request = Request::get("https://cloudflare.com").body(()).unwrap();
+    // println!("{:?}", request);
+    // println!("{:?}", request.headers());
+    // send(request)
+
+    let mut addrs_iter = "cloudflare.com:443".to_socket_addrs().unwrap();
+    let ip = addrs_iter.next().unwrap();
+    println!("{:?}", ip);
+
+    // let client = blocking::Client::new();
+    // let mut get = client.get("https://cloudflare.com");
+    // let mut before = SystemTime::now();
+    // let mut response = get.send();
+    // let mut after = SystemTime::now();
+    // println!("{:?}", after.duration_since(before));
+    // get = client.get("https://cloudflare.com");
+    // before = SystemTime::now();
+    // response = get.send();
+    // println!("{:?}", response);
+    // after = SystemTime::now();
+    // println!("{:?}", after.duration_since(before));
+    // let get_localhost = client.get("http://localhost");
+    // before = SystemTime::now();
+    // response = get_localhost.send();
+    // after = SystemTime::now();
+    // println!("localhost {:?}", after.duration_since(before));
+    //let response = reqwest::blocking::get("https://cloudflare.com")?.text(); //.json::<HashMap<String, String>>()?;
+
+    let localhost_v4 = IpAddr::V4(Ipv4Addr::new(104, 16, 133, 229)); // this should be ip variable, but formatted
+    // let localhost_v4 = IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1));
+    let mut now = SystemTime::now();
+    let ping = icmp::IcmpSocket::connect(localhost_v4);
+    let mut after = SystemTime::now();
+    println!("{:?}, {:?}, {:?}", now, after, after.duration_since(now));
+    let mut ping = ping.unwrap();
+    let payload: &[u8] = &[1, 2];
+    let mut now = SystemTime::now();
+    let result = ping.send(payload);
+    let mut after = SystemTime::now();
+    println!( //result is strange, because time is very short, like 39.238µs
+        "{:?}, {:?}, {:?}, {:?}",
+        result,
+        now,
+        after,
+        after.duration_since(now)
+    );
+    let result = ping.send(&[1, 2, 3]);
+    println!("{:?}", result);
 }
