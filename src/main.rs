89fcabddb22ce12d523551d51292359dceff5db5
jmaczan <jedrzejpawel@maczan.pl>
2022-08-09 21:43:20 +0200

    Clean up the code

--- src/main.rs
+++ src/main.rs
@@ -6,18 +6,7 @@ use std::time::SystemTime;
 fn main() {
     let mut addrs_iter = "cloudflare.com:443".to_socket_addrs().unwrap();
     let raw_ip = addrs_iter.next().unwrap().to_string();
-    let mut ip = raw_ip.split(".").collect::<Vec<_>>();
-    ip[3] = ip[3].split(":").collect::<Vec<_>>()[0];
-    println!("{:?}", ip);
-    println!(
-        "{} {} {} {}",
-        String::from(ip[0]).parse::<u8>().unwrap(),
-        String::from(ip[1]).parse::<u8>().unwrap(),
-        String::from(ip[2]).parse::<u8>().unwrap(),
-        String::from(ip[3].split(":").collect::<Vec<_>>()[0])
-            .parse::<u8>()
-            .unwrap()
-    );
+    let ip = raw_ip.split(".").collect::<Vec<_>>();
     let ip_to_ping = IpAddr::V4(Ipv4Addr::new(
         String::from(ip[0]).parse::<u8>().unwrap(),
         String::from(ip[1]).parse::<u8>().unwrap(),
@@ -27,23 +16,44 @@ fn main() {
             .unwrap(),
     ));
     let icmp_before_connect_timestamp = SystemTime::now();
+    let icmp_socket = icmp::IcmpSocket::connect(ip_to_ping);
     let icmp_after_connect_timestamp = SystemTime::now();
-    println!(
-        "{:?}, {:?}, {:?}",
-        icmp_before_connect_timestamp,
-        icmp_after_connect_timestamp,
-        icmp_after_connect_timestamp.duration_since(icmp_before_connect_timestamp)
-    );
-    let mut ping = icmp::IcmpSocket::connect(ip_to_ping).unwrap();
-    let ping_payload: &[u8] = &[1, 2]; // random payload
+    match &icmp_socket {
+        Ok(_) => println!(
+            "It took {:?} seconds to connect with server",
+            icmp_after_connect_timestamp
+                .duration_since(icmp_before_connect_timestamp)
+                .unwrap()
+                .as_secs_f64()
+        ),
+        Err(err) => {
+            println!("Socket connection failed. {}", err);
+            return;
+        }
+    }
+    let ping_payload: &[u8] = &[1, 2]; // meaningless payload
     let icmp_before_ping_timestamp = SystemTime::now();
-    let ping_result = ping.send(ping_payload);
+    let ping_result = icmp_socket.unwrap().send(ping_payload);
     let icmp_after_ping_timestamp = SystemTime::now();
-    println!(
-        "{:?}, {:?}, {:?}, {:?}",
-        ping_result.unwrap(),
-        icmp_before_ping_timestamp,
-        icmp_after_ping_timestamp,
-        icmp_after_ping_timestamp.duration_since(icmp_before_ping_timestamp)
-    );
+    match ping_result {
+        Ok(_) => {
+            println!(
+                "Ping took {:?} seconds",
+                icmp_after_ping_timestamp
+                    .duration_since(icmp_before_ping_timestamp)
+                    .unwrap()
+                    .as_secs_f64()
+            );
+        }
+        Err(_) => {
+            println!(
+                "Ping failed. It took {:?} seconds",
+                icmp_after_ping_timestamp
+                    .duration_since(icmp_before_ping_timestamp)
+                    .unwrap()
+                    .as_secs_f64()
+            );
+            return;
+        }
+    }
 }
