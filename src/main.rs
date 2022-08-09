7e1c06174b064cf12d3418f7c44276468ae51497
jmaczan <jedrzejpawel@maczan.pl>
2022-08-09 21:24:32 +0200

    Resolve IP address of a given domain and port, connect to it using ICMP socket and send ping. Measure how long it takes to establish the connection and to send the ping.

--- src/main.rs
+++ src/main.rs
@@ -1,59 +1,49 @@
-use http::Request;
 use icmp;
-use reqwest::blocking;
-use std::collections::HashMap;
+use std::net::ToSocketAddrs;
 use std::net::{IpAddr, Ipv4Addr};
-use std::net::{SocketAddr, ToSocketAddrs};
 use std::time::SystemTime;
 
 fn main() {
-    // println!("Hello, world!");
-    // let request = Request::get("https://cloudflare.com").body(()).unwrap();
-    // println!("{:?}", request);
-    // println!("{:?}", request.headers());
-    // send(request)
-
     let mut addrs_iter = "cloudflare.com:443".to_socket_addrs().unwrap();
-    let ip = addrs_iter.next().unwrap();
+    let raw_ip = addrs_iter.next().unwrap().to_string();
+    let mut ip = raw_ip.split(".").collect::<Vec<_>>();
+    ip[3] = ip[3].split(":").collect::<Vec<_>>()[0];
     println!("{:?}", ip);
-
-    // let client = blocking::Client::new();
-    // let mut get = client.get("https://cloudflare.com");
-    // let mut before = SystemTime::now();
-    // let mut response = get.send();
-    // let mut after = SystemTime::now();
-    // println!("{:?}", after.duration_since(before));
-    // get = client.get("https://cloudflare.com");
-    // before = SystemTime::now();
-    // response = get.send();
-    // println!("{:?}", response);
-    // after = SystemTime::now();
-    // println!("{:?}", after.duration_since(before));
-    // let get_localhost = client.get("http://localhost");
-    // before = SystemTime::now();
-    // response = get_localhost.send();
-    // after = SystemTime::now();
-    // println!("localhost {:?}", after.duration_since(before));
-    //let response = reqwest::blocking::get("https://cloudflare.com")?.text(); //.json::<HashMap<String, String>>()?;
-
-    let localhost_v4 = IpAddr::V4(Ipv4Addr::new(104, 16, 133, 229)); // this should be ip variable, but formatted
-    // let localhost_v4 = IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1));
-    let mut now = SystemTime::now();
-    let ping = icmp::IcmpSocket::connect(localhost_v4);
-    let mut after = SystemTime::now();
-    println!("{:?}, {:?}, {:?}", now, after, after.duration_since(now));
-    let mut ping = ping.unwrap();
-    let payload: &[u8] = &[1, 2];
-    let mut now = SystemTime::now();
-    let result = ping.send(payload);
-    let mut after = SystemTime::now();
-    println!( //result is strange, because time is very short, like 39.238µs
-        "{:?}, {:?}, {:?}, {:?}",
-        result,
-        now,
-        after,
-        after.duration_since(now)
+    println!(
+        "{} {} {} {}",
+        String::from(ip[0]).parse::<u8>().unwrap(),
+        String::from(ip[1]).parse::<u8>().unwrap(),
+        String::from(ip[2]).parse::<u8>().unwrap(),
+        String::from(ip[3].split(":").collect::<Vec<_>>()[0])
+            .parse::<u8>()
+            .unwrap()
     );
-    let result = ping.send(&[1, 2, 3]);
-    println!("{:?}", result);
+    let ip_to_ping = IpAddr::V4(Ipv4Addr::new(
+        String::from(ip[0]).parse::<u8>().unwrap(),
+        String::from(ip[1]).parse::<u8>().unwrap(),
+        String::from(ip[2]).parse::<u8>().unwrap(),
+        String::from(ip[3].split(":").collect::<Vec<_>>()[0])
+            .parse::<u8>()
+            .unwrap(),
+    ));
+    let icmp_before_connect_timestamp = SystemTime::now();
+    let icmp_after_connect_timestamp = SystemTime::now();
+    println!(
+        "{:?}, {:?}, {:?}",
+        icmp_before_connect_timestamp,
+        icmp_after_connect_timestamp,
+        icmp_after_connect_timestamp.duration_since(icmp_before_connect_timestamp)
+    );
+    let mut ping = icmp::IcmpSocket::connect(ip_to_ping).unwrap();
+    let ping_payload: &[u8] = &[1, 2]; // random payload
+    let icmp_before_ping_timestamp = SystemTime::now();
+    let ping_result = ping.send(ping_payload);
+    let icmp_after_ping_timestamp = SystemTime::now();
+    println!(
+        "{:?}, {:?}, {:?}, {:?}",
+        ping_result.unwrap(),
+        icmp_before_ping_timestamp,
+        icmp_after_ping_timestamp,
+        icmp_after_ping_timestamp.duration_since(icmp_before_ping_timestamp)
+    );
 }
