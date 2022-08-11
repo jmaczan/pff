a3e45087f52f52a0e752c6505f1911685ef2af73
jmaczan <jedrzejpawel@maczan.pl>
2022-08-11 21:54:37 +0200

    Don't panic when domain is not resolved

--- src/main.rs
+++ src/main.rs
@@ -3,74 +3,85 @@ use fastping_rs::PingResult::{Idle, Receive};
 use fastping_rs::Pinger;
 use spinners::{Spinner, Spinners};
 use std::cmp::Ordering;
-use std::io::{stdout, Write};
 use std::net::ToSocketAddrs;
 use std::time::Duration;
 
-fn resolve_domain_ip(domain: &str) -> String {
+fn resolve_domain_ip(domain: &str) -> Result<String, std::io::Error> {
     let addrs_iter = domain.to_socket_addrs();
-    match &addrs_iter {
-        Ok(_) => {}
-        Err(err) => {
-            panic!(
-                "An error occured when resolving a domain name: \"{}\"",
-                err.to_string().bright_red()
-            );
+    match addrs_iter {
+        Ok(_) => {
+            let raw_ip = addrs_iter.unwrap().next().unwrap().to_string();
+            let ip = raw_ip.split(":").collect::<Vec<_>>();
+            Ok(ip[0].to_string())
         }
+        Err(err) => Err(err),
     }
-    let raw_ip = addrs_iter.unwrap().next().unwrap().to_string();
-    let ip = raw_ip.split(":").collect::<Vec<_>>();
-    ip[0].to_string()
 }
 
 fn print_average_ping(string: ColoredString, average_ping: Duration) {
     print!(
-        "\r{} ({:?})                          ",
+        "\rYour Internet connection is {} ({:?} ping on average)",
         string, average_ping
     );
     println!();
 }
 
-fn print_result(average_ping: Duration, total_trials: u32) {
+fn print_result(average_ping: Duration, total_trials: u32, ping_drops: u32) {
     let average_ping = average_ping / total_trials;
 
     if (average_ping).cmp(&Duration::from_millis(25)) == Ordering::Less {
-        print_average_ping("Excellent".bright_green().bold(), average_ping);
+        print_average_ping("excellent".bright_green().bold(), average_ping);
     } else if average_ping.cmp(&Duration::from_millis(100)) == Ordering::Less {
-        print_average_ping("Good".green().bold(), average_ping);
+        print_average_ping("good".green().bold(), average_ping);
     } else if average_ping.cmp(&Duration::from_millis(500)) == Ordering::Less {
-        print_average_ping("Average".yellow().bold(), average_ping);
+        print_average_ping("average".yellow().bold(), average_ping);
     } else if average_ping.cmp(&Duration::from_millis(1000)) == Ordering::Less {
-        print_average_ping("Bad".bright_red().bold(), average_ping);
+        print_average_ping("bad".bright_red().bold(), average_ping);
     } else {
-        print_average_ping("Really bad".red().bold(), average_ping);
+        print_average_ping("really bad".red().bold(), average_ping);
+    }
+}
+
+fn create_pinger() -> (
+    fastping_rs::Pinger,
+    std::sync::mpsc::Receiver<fastping_rs::PingResult>,
+) {
+    match Pinger::new(None, Some(56)) {
+        Ok((pinger, results)) => (pinger, results),
+        Err(e) => panic!("Error creating pinger: {}", e),
     }
 }
 
 fn main() {
-    let (pinger, results) = match Pinger::new(None, Some(56)) {
-        Ok((pinger, results)) => (pinger, results),
-        Err(e) => panic!("Error creating pinger: {}", e),
+    let (pinger, results) = create_pinger();
+    let ip = match resolve_domain_ip("cloudflare.com:443") {
+        Ok(ip) => ip,
+        Err(err) => {
+            println!(
+                "An error occured when resolving a domain name: \"{}\"",
+                err.to_string().bright_red()
+            );
+            return;
+        }
     };
-    let ip = resolve_domain_ip("cloudflare.com:443");
+
     pinger.add_ipaddr(&ip);
     pinger.run_pinger();
 
     let mut average_ping = Duration::new(0, 0);
     let total_trials = 3;
     let mut trials_left = total_trials;
-    let mut stdout = stdout();
-    let mut spinner = Spinner::new(Spinners::Dots9, "I'm checking your connection".into());
+    let mut spinner = Spinner::new(Spinners::Dots9, "I'm examining your ping".into());
+    let mut ping_drops = 0;
     loop {
         if trials_left == 0 {
             break;
         }
-        stdout.flush().unwrap();
         trials_left -= 1;
         match results.recv() {
             Ok(result) => match result {
-                Idle { addr } => {
-                    println!("Idle Address {}.", addr);
+                Idle { addr: _ } => {
+                    ping_drops += 1;
                 }
                 Receive { addr: _, rtt } => {
                     average_ping = average_ping.saturating_add(rtt);
@@ -81,5 +92,5 @@ fn main() {
     }
     spinner.stop();
 
-    print_result(average_ping, total_trials);
+    print_result(average_ping, total_trials, ping_drops);
 }
